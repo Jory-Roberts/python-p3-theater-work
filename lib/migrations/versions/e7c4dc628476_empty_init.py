d6a12a412567e676358291bf710a0ddb73899904
Jory Roberts <robertsjory13@gmail.com>
2023-08-08 08:23:44 -0500

    Initial setup

---
+++ lib/migrations/versions/e7c4dc628476_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: e7c4dc628476
+Revises: 
+Create Date: 2023-08-08 08:21:13.203697
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e7c4dc628476'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,12 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
 metadata = MetaData(naming_convention=convention)
 
+engine = create_engine("sqlite:///theater.db")
 Base = declarative_base(metadata=metadata)
